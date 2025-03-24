5a7a5760d76fd265427f2db0d8afcda0085b2b3c
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2025-03-24 11:02:08 +0100

    Implemented complex numbers

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,7 @@
 package tasks.adts
 
+import scala.math.abs
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -20,11 +22,18 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = (Double, Double)
+
+    def complex(re: Double, im: Double): Complex = (re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex._1
+      def im(): Double = complex._2
+      def sum(other: Complex): Complex = (complex._1 + other._1, complex._2 + other._2)
+      def subtract(other: Complex): Complex = (complex._1 - other._1, complex._2 - other._2)
+      def asString(): String =
+        val sign = if complex._2 < 0 then "-" else "+"
+        complex match
+          case i if complex._1 == 0 & complex._2 == 0 => "0.0"
+          case j if complex._1 != 0 & complex._2 != 0 => complex._1 + " " + sign + " " + abs(complex._2) + "i"
+          case k if complex._1 == 0 => complex._2 + "i"
+          case _ => complex._1 + ""
