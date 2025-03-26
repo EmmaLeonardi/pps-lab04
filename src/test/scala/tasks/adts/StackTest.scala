ee88a4f57deb3bd6a6d408436e59c05a296bc354
Emma Leonardi <eleonardi.mobile@gmail.com>
2025-03-26 20:24:13 +0100

    Implemented stackADT

--- src/test/scala/tasks/adts/StackTest.scala
+++ src/test/scala/tasks/adts/StackTest.scala
@@ -3,7 +3,8 @@ package tasks.adts
 import org.junit.*
 import org.junit.Assert.*
 import tasks.adts.Ex3Stacks.StackImpl
-import u03.Sequences.Sequence
+import u03.extensionmethods.Sequences.Sequence.*
+import u03.extensionmethods.Sequences.*
 import u03.Optionals.Optional
 
 /* Tests should be clear, but note they are expressed independently of the 
